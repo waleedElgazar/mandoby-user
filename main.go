0fa21f6c5d8f2cff678f6d85d732998e92ccc2b6
waleed reda <walidreda428@gmail.com>
2021-07-01 19:04:02 +0200

    user methods finished

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"demo/functions"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("POORT")
 	router := mux.NewRouter()
 	router.HandleFunc("/addUser", functions.AddUser).Methods("POST")
 	router.HandleFunc("/getUser", functions.GetUser).Methods("GET")
 	router.HandleFunc("/deleteUser", functions.DelteUser).Methods("DELETE")
 	router.HandleFunc("/updateUser", functions.UpdateUSer).Methods("PUT")
 	router.HandleFunc("/getAllUsers", functions.GetUsers).Methods("GET")
-	http.ListenAndServe(":8082", router)
+	http.ListenAndServe(":"+port, router)
 
 }
